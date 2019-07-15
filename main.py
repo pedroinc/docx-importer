22097f75140dbd5a156428c4bd3ab092bbaffda7
Pedro Soares <pedro.soares.lacerda@tvglobo.com.br>
2019-07-15 15:59:02 -0300

    iterating over all the files

--- main.py
+++ main.py
@@ -7,46 +7,40 @@ from pprint import pprint
 if __name__ == "__main__":
     # DocConverter().convert_docs_to_docx('docs', 'docx')   
     docs_folder = '/home/placerda/Downloads/pessoal/docs_ofi/all_docx/'
-    # try:
-    #     base_path = os.getcwd()
-    #     files = os.listdir("{0}/{1}".format(base_path, from_folder))
+    
+    try:
+        # base_path = os.getcwd()
+        files = os.listdir(docs_folder)
+        counter = 0
 
-    #     for filename in files:
-    #         pprint(filename)
+        for filename in files:
+            pprint('{} : {}'.format(counter, filename))
+            counter = counter + 1
+            # break
 
-    # Exception as e:
-    #     pprint(e)    
+            # doc = Document('{}filename'.format(docs_folder))
+            # doc_processor = DocProcessor(doc)
 
-    doc = Document('{}AII 8224 01.06.2010.docx'.format(docs_folder))
-    doc_processor = DocProcessor(doc)
+            # data = doc_processor.extract_data()
+            # table_itens = chain(data['table_itens'])    
 
-    data = doc_processor.extract_data()
-    table_itens = chain(data['table_itens'])    
+            # service_itens = [{ 
+            #                     "id": int(item['ITEM']), 
+            #                     "description": item['DESCRIÇÃO DO SERVIÇOS'],
+            #                     "price": float(item['PREÇO']
+            #                                 .replace('.', '')
+            #                                 .replace(',', '.')) 
+            #                                 if item['PREÇO'].strip() != ''  
+            #                                 else '',
+            #                 }
+            #                 # it
+            #                 for item in table_itens 
+            #                 if 'DESCRIÇÃO DO SERVIÇOS' in item]
 
-    service_itens = [{ 
-                        "id": int(item['ITEM']), 
-                        "description": item['DESCRIÇÃO DO SERVIÇOS'],
-                        "price": float(item['PREÇO']
-                                    .replace('.', '')
-                                    .replace(',', '.')) 
-                                    if item['PREÇO'].strip() != ''  
-                                    else '',
-                    }
-                    # it
-                    for item in table_itens 
-                    if 'DESCRIÇÃO DO SERVIÇOS' in item]
+            # parts_itens = [item for item in table_itens]
 
-    parts_itens = [item for item in table_itens]
-    # parts_itens = [item for item in table_itens if 'GENUÍNA' in item]
+            # pprint(service_itens)
+            # pprint(parts_itens)
 
-
-pprint(service_itens)
-pprint(parts_itens)
-    # print(match)
-    # read_tables(doc)
-
-
-
-
-
-
+    except Exception as e:
+        pprint(e)
