79845b91648f826380a473b18f353a7636da0a8e
Pedro Soares <pedroinc@gmail.com>
2019-09-11 02:21:59 -0300

    conflict fix

--- doc_processor.py
+++ doc_processor.py
@@ -1,12 +1,8 @@
 import re
-<<<<<<< HEAD
 from docx import Document
-import subprocess
 import os
-=======
 from enum import Enum
 from datetime import datetime
->>>>>>> feature/data-extractor-improvements
 
 class RegexFieldTypes:
     #SERVICE_DATE = r'\d{2}.\d{2}.\d{4}'
@@ -16,63 +12,6 @@ class RegexFieldTypes:
     PHONE = r'(TEL.:)(.*)([0-9/ ])'    
     CUSTOMER = r'(PROP.:)(.*)'    
 
-<<<<<<< HEAD
-class DocProcessor:
-    
-    LICENSE_PLATE_IN_DOC_PATTERN = r"(.*)([a-zA-Z]{3}\s\d{4})(.*)$"
-    LICENSE_PLATE_FILENAME_PATTERN = r"([a-zA-Z]{3}\s\d{4})"
-    DATE_PATTERN = r"([0-9]{2}).([0-9]{2}).([0-9]{4})"
-    CUSTOMER_NAME_PATTERN = r"(PROP.: )(.+)"
-
-    def __init__(self, file_path):
-        self._docx = Document(file_path)
-        self._file_lines = self.read_lines()
-
-
-    def read_license_plate(self):
-        for line in self._file_lines:
-            match = re.match(DocProcessor.LICENSE_PLATE_IN_DOC_PATTERN, line)
-            if match:
-                return match.group(2)
-        return None
-
-    def read_customer_name(self):
-        for line in self._file_lines:
-            match = re.match(DocProcessor.CUSTOMER_NAME_PATTERN, line)
-            if match:
-                return match.group(2)
-        return None     
-
-    def read_date(self):
-        for line in self._file_lines:
-            match = re.match(DocProcessor.DATE_PATTERN, line)
-            if match:
-                return "{}/{}/{}".format(match.group(1), match.group(2), match.group(3))
-        return None      
-
-    # @staticmethod
-    # def normalize_names():
-    #     pass
-
-    # @staticmethod
-    # def convert_docs_to_docx(from_folder, to_folder):
-    #     try:
-    #         # base_path = os.getcwd()
-    #         # files = os.listdir("{0}/{1}".format(base_path, from_folder))
-
-    #         for filename in os.listdir(from_folder):
-    #             if filename.endswith('.doc'):
-    #                 path_to_file = '{}/{}'.format(from_folder, filename)
-    #                 subprocess.call(
-    #                     ['soffice', '--headless', '--convert-to',
-    #                     'docx', '--outdir', to_folder, path_to_file]
-    #                 )
-    #     except Exception as e:
-    #         print(e)
-
-    def read_tables(self):
-        for table in self._docx.tables:
-=======
 class DocProcessor:    
     def __init__(self, docx_obj):
         self._docx = docx_obj
@@ -81,7 +20,6 @@ class DocProcessor:
     def read_table_content(doc_tables):
         content_lines = []
         for table in doc_tables:         
->>>>>>> feature/data-extractor-improvements
             for i, row in enumerate(table.rows):
                 text = (cell.text for cell in row.cells)
 
@@ -98,10 +36,6 @@ class DocProcessor:
                 # print(row_data)
         return content_lines
 
-<<<<<<< HEAD
-
-=======
->>>>>>> feature/data-extractor-improvements
     def read_lines(self):
         lines = []
         index = 0
@@ -112,10 +46,6 @@ class DocProcessor:
             index = index + 1
         return lines
 
-<<<<<<< HEAD
-    def file_name_valid(self, filename):
-        return re.match(DocProcessor.LICENSE_PLATE_FILENAME_PATTERN, filename)
-=======
     def read_phones(self, lines):
         pattern = RegexFieldTypes.PHONE  
         for line in lines:
@@ -152,7 +82,6 @@ class DocProcessor:
     def extract_data(self):
         lines = self.read_lines()
         phones = self.read_phones(lines)
->>>>>>> feature/data-extractor-improvements
 
         return {
             "license_plate" : DocProcessor.read_field(lines, RegexFieldTypes.LICENSE_PLATE, 2),
--- main.py
+++ main.py
@@ -1,43 +1,5 @@
 from doc_processor import DocProcessor
 from docx import Document
-<<<<<<< HEAD
-import re
-import os
-
-if __name__ == "__main__":
-
-    try:
-        # origin = '/home/placerda/Downloads/mecanica/docs'
-        documents_path = '/home/placerda/Downloads/mecanica/all_docx'
-        # DocProcessor().convert_docs_to_docx(origin, destin)
-
-        my_documents = []
-        from_folder = 'docx'
-        
-        base_path = os.getcwd()
-        files = os.listdir(documents_path)
-        i = 0
-        for filename in files:
-            if i >= 500:
-                print("*************** {} itens".format(i))
-                break
-
-            # path_to_file = '{}/{}/{}'.format(base_path, from_folder, filename)
-            # print(path_to_file)
-            # print(filename)
-            file = DocProcessor("{}/{}".format(documents_path, filename))
-            # print(file.read_license_plate())
-            if file.read_license_plate() or file.read_customer_name():
-                print("{}, {}".format(file.read_license_plate(), file.read_customer_name()))
-
-            # if file.read_date():
-            #     print(file.read_date())
-            # print(file.read_lines())    
-            i = i + 1
-
-    except Exception as e:
-        print(e)
-=======
 import os
 
 
@@ -78,4 +40,3 @@ if __name__ == "__main__":
             counter = counter + 1
         # print(match)
         # read_tables(doc)
->>>>>>> feature/data-extractor-improvements
