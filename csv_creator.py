d625a9fc5cbcde5a136d0b6b4fad4680392e0d92
Pedro Soares <pedro.soares.lacerda@tvglobo.com.br>
2021-02-03 13:31:05 -0300

    removing dot sequence from docx

--- csv_creator.py
+++ csv_creator.py
@@ -5,9 +5,9 @@ import os
 class CsvCreator:
     def __init__(self, filename, docs_folder):
         self.filename = filename
-        self.docs_folder = docs_folder        
+        self.docs_folder = docs_folder
 
-    def create(self):                
+    def create(self):
         file_document = open(self.filename,'w')
         files = os.listdir(self.docs_folder)
         file_document.write("LICENSE_PLATE, VEHICLE_NAME, VEHICLE_NUMBER, PHONE_1, PHONE_2, CLIENT, SERVICE_DATE \n")
@@ -17,14 +17,14 @@ class CsvCreator:
         for filename in files:
             print(filename)
     #        if counter >= 3000:
-    #            break        
-            doc = Document('{}{}'.format(self.docs_folder, filename))
-            doc_processor = DocProcessor(doc)
+    #            break
+
+            doc_processor = DocProcessor(self.docs_folder, filename)
 
             data = doc_processor.extract_data()
 
             plate = data['license_plate']
-            vehicle_name = data['vehicle_name']       
+            vehicle_name = data['vehicle_name']
             vehicle_number = data['vehicle_number']
             phone1, phone2 = data['phone_numbers']
             customer = data['customer']
@@ -32,7 +32,7 @@ class CsvCreator:
 
             if plate != 'CAMPO_VAZIO':
                 file_document.write("{},{},{},{},{},{} \n".format(
-                    plate, 
+                    plate,
                     vehicle_name,
                     vehicle_number,
                     phone1,
@@ -41,3 +41,4 @@ class CsvCreator:
                     date))
 
                 counter = counter + 1
+            break
--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from csv_creator import CsvCreator
 import os
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
+    # import pdb; pdb.set_trace()
+
     filename = os.getenv('CSV_NAME')
-    docs_folder = os.getenv('DOCS_FOLDER')    
-    
+    docs_folder = os.getenv('DOCS_FOLDER')
+
     csv_creator = CsvCreator(filename, docs_folder)
     print('creating the csv with the data.')
     csv_creator.create()
